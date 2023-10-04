86c6866b5c8d6778dc0d1e43a8c95e8bf24d537e
Mario M <mariomorenoarroyo01@gmail.com>
2023-10-04 12:42:59 +0200

    Boletin3

---
+++ bol3_EJ2/app/src/main/java/com/mariomorenoarroyo/bol3_ej2/ImageAdapter.kt
@@ -0,0 +1,43 @@
+package com.mariomorenoarroyo.bol3_ej2
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter(private val context: Context, private val imageList: List<String>) :
+    RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+
+                    val location = "geo:0,0?q=${imageList[position]}"
+                    val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(location))
+                    context.startActivity(mapIntent)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+}
